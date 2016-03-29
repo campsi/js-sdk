1dd8d3fa362de53d071224acf2a799702f0af5ac
romainbessugesmeusy <romainbessuges@gmail.com>
2016-03-29 15:31:19 +0200

    Pizzeria homemade

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,19 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
+var stylus = require('gulp-stylus');
+var autoprefixer = require('gulp-autoprefixer');
 
 var b = watchify(browserify({entries: ['./index.js'], debug: false}));
 
-gulp.task('default', ['bundle', 'compress']);
+gulp.task('default', ['watch', 'bundle', 'compress']);
 
+gulp.task('watch', function () {
+    gulp.watch('**/*.styl', ['stylus']);
+});
 gulp.task('bundle', function bundle() {
     return b.bundle()
         .pipe(source('campsi-sdk.js'))
@@ -29,4 +33,17 @@ gulp.task('compress', function compress() {
         .pipe(buffer())
         .pipe(streamify(uglify()))
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('stylus', function () {
+    gulp.src('./examples/arezzo/main.styl')
+        .pipe(stylus({
+            compress: false,
+            'include css': true
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./examples/arezzo'));
 });
